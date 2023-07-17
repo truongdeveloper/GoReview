83c3b3d5d2ec4db9eaff003c5d722ca6dbe796b9
truongdeveloper <vanminhtruong678@gmail.com>
2023-07-18 00:10:19 +0700

    Add UI

--- GoReview/Program.cs
+++ GoReview/Program.cs
@@ -1,7 +1,12 @@
+using GoReview.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
