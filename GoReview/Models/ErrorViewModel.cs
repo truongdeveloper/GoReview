97656f5ac25725bebdb3c475f8e5d393c9542df2
truong <vanminhtruong678@gmail.com>
2023-07-21 10:42:35 +0700

    Config gitignone

--- GoReview/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GoReview.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
