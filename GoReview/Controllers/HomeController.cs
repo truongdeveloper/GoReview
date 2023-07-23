991dfa9ca6ef131e83a4da5f1a5e66074525a192
truongdeveloper <vanminhtruong678@gmail.com>
2023-07-24 03:55:57 +0700

    Search by Ajax

--- GoReview/Controllers/HomeController.cs
+++ GoReview/Controllers/HomeController.cs
@@ -18,50 +18,59 @@ namespace GoReview.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> IndexAsync()
+        public async Task<IActionResult> Index(string keyword)
         {
-            var _post = _context.Posts.Include(p => p.Cat).Include(p => p.User).Include(p => p.Feedbacks);
-
-            if(User.Identity.IsAuthenticated)
-            {
-                int loggedInUserId = Int32.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
-
-                foreach (var post in _post)
-                {
-                    // Kiểm tra xem người dùng đã like bài viết hay chưa
-                    post.IsLiked = post.Feedbacks.Any(f => f.UserId == loggedInUserId && f.Like == true);
-                }
-            }
-
-
-            return View(_post);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> IndexAsync(string keyword)
-        {
-            var _post = _context.Posts.Include(p => p.Cat).Include(p => p.User).Include(p => p.Feedbacks);
+            IQueryable<Post> query = _context.Posts.Include(p => p.Cat).Include(p => p.User).Include(p => p.Feedbacks);
 
             if (User.Identity.IsAuthenticated)
             {
                 int loggedInUserId = Int32.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
 
-                foreach (var post in _post)
+                foreach (var post in query)
                 {
                     // Kiểm tra xem người dùng đã like bài viết hay chưa
-                    post.IsLiked = post.Feedbacks.Any(f => f.User.UserId == loggedInUserId && f.Like == true);
+                    post.IsLiked = _context.Feedbacks.Any(f => f.UserId == loggedInUserId && f.Like == true);
                 }
             }
 
-            List<Post> postFillter = _post.Where(p => p.Title.Contains(keyword)).ToList();
-
-
-            return Json(new
+            if (!string.IsNullOrEmpty(keyword))
             {
-                Title = "truong",
-                Content = "truong"
-            });
+                // Nếu có từ khóa tìm kiếm, thực hiện lọc bài viết theo từ khóa
+                query = query.Where(p => p.Title.Contains(keyword));
+                var postsSearch = await query.ToListAsync();
+                return PartialView(postsSearch);
+            }
+
+            var posts = await query.ToListAsync();
+            // Thêm các xử lý cần thiết, ví dụ như tính toán số lượng lượt thích, số lượng bình luận, ...
+            return View(posts);
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> IndexAsync(string keyword)
+        //{
+        //    var _post = _context.Posts.Include(p => p.Cat).Include(p => p.User).Include(p => p.Feedbacks);
+
+        //    if (User.Identity.IsAuthenticated)
+        //    {
+        //        int loggedInUserId = Int32.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+        //        foreach (var post in _post)
+        //        {
+        //            // Kiểm tra xem người dùng đã like bài viết hay chưa
+        //            post.IsLiked = post.Feedbacks.Any(f => f.User.UserId == loggedInUserId && f.Like == true);
+        //        }
+        //    }
+
+        //    List<Post> postFillter = _post.Where(p => p.Title.Contains(keyword)).ToList();
+
+
+        //    return Json(new
+        //    {
+        //        Title = "truong",
+        //        Content = "truong"
+        //    });
+        //}
 
 
 
